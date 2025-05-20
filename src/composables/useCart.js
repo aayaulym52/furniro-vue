6d81cd36ba095bb2cb8d9c41086da717aedfea01
Aiya <aayaulym52@gmail.com>
2025-05-20 21:21:04 +0500

    Update useCart.js

--- src/composables/useCart.js
+++ src/composables/useCart.js
@@ -1,30 +1,19 @@
 import { ref, watch } from "vue";
 
-// Загружаем корзину из localStorage при запуске
 const storedCart = JSON.parse(localStorage.getItem("cart")) || [];
 const cart = ref(storedCart);
-
-// Проверка есть ли товар в корзине
 const isInCart = (id) => cart.value.some((item) => item.id === id);
-
-// Добавить в корзину
 const addToCart = (item) => {
   if (!isInCart(item.id)) {
     cart.value.push(item);
   }
 };
-
-// Удалить из корзины
 const removeFromCart = (item) => {
   cart.value = cart.value.filter((cartItem) => cartItem.id !== item.id);
 };
-
-// Кнопка плюс — добавляет или удаляет
 const onClickAddPlus = (item) => {
   isInCart(item.id) ? removeFromCart(item) : addToCart(item);
 };
-
-// Следим за корзиной и сохраняем в localStorage
 watch(
   cart,
   () => {
@@ -33,7 +22,6 @@ watch(
   { deep: true }
 );
 
-// Экспорт
 export function useCartActions() {
   return {
     cart,
